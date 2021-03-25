565f8296dd5d3f8dac737249e3a4fbe50a399bec
Víctor Miranda <victormiranda13v@gmail.com>
2021-03-25 14:41:24 -0300

    Create Component Mains

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles'
+import Layout from './components/Layout'
+
+
+
+const App: React.FC = () => {
+    return (
+        <>
+        <GlobalStyles/>
+        <Layout/> 
+        </>
+    )
+
+}
+
+export default App;
---
+++ src/components/Content/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+ export const Container = styled.div`
+ 
+    grid-area: CT;
+    background-color: #1b1f38;
+    
+ `;
